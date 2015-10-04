d6a0d452a2006b6b68ffc393b66db833af9f28ca
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-03 17:48:29 -0700

    Completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  before do
+    Post.destroy_all
+    @post = Post.create!( title: "title", body: "body" )
+  end
 
   describe "GET #index" do
     it "returns http success" do
@@ -9,9 +13,8 @@ RSpec.describe PostsController, type: :controller do
     end
 
     it "assigns [my_post] to @posts" do
-      my_post = Post.create!(title: "Title", body: "Body")
       get :index
-      expect(assigns(:posts)).to eq([my_post])
+      expect(assigns(:posts)).to eq([@post])
     end
   end
 
@@ -32,57 +35,92 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-#  describe "GET #create" do
-#    it "returns http success" do
-#      get :create
-#      expect(response).to have_http_status(:success)
-#    end
-#
-#    it "increases the number of Post by 1" do
-#      expect{post :create, post: {title: "Title", body: "Body"}}.to change(Post,:count).by(1)
-#    end
-#
-#    it "assigns the new post to @post" do
-#        post :create, post: {title: "Title", body: "Body"}
-#        expect(assigns(:post)).to eq Post.last
-#    end
-#  end
+  describe "GET #create" do
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: "Title", body: "Body"}}.to change(Post,:count).by(1)
+    end
+
+    it "assigns the new post to @post" do
+      post :create, post: {title: "Title", body: "Body"}
+      expect(assigns(:post)).to eq Post.last
+    end
+
+    it "redirects to the new post" do
+      post :create, post: {title: "Title", body: "Body"}
+      expect(response).to redirect_to Post.last
+    end
+  end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: @post.id}
       expect(response).to have_http_status(:success)
     end
 
     it "renders the show view" do
-      get :show
+      get :show, {id: @post.id}
       expect(response).to render_template :show
     end
 
     it "assigns my_post to @post" do
-      expect(assigns(:post)).to eq(my_post)
+      get :show, {id: @post.id}
+      expect(assigns(:post)).to eq(@post)
     end
   end
 
-#  describe "GET #edit" do
-#    it "returns http success" do
-#      get :edit
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
-#
-#  describe "GET #update" do
-#    it "returns http success" do
-#      get :update
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
-#
-#  describe "GET #destroy" do
-#    it "returns http success" do
-#      get :destroy
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, {id: @post.id}
+      expect(response).to have_http_status(:success)
+    end
 
+    it "renders the edit view" do
+      get :edit, {id: @post.id}
+      expect(response).to render_template :edit
+    end
+
+    it "assigns post to be updated to @post" do
+      get :edit, {id: @post.id}
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq @post.id
+      expect(post_instance.title).to eq @post.title
+      expect(post_instance.body).to eq @post.body
+    end
+  end
+
+  describe "GET #update" do
+    it "updates post with expected attributes" do
+      new_title = "New Title"
+      new_body = "New Body"
+
+      put :update, id: @post.id, post: {title: new_title, body: new_body}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq @post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = "New Title"
+      new_body = "New Body"
+
+      put :update, id: @post.id, post: {title: new_title, body: new_body}
+      expect(response).to redirect_to @post
+    end
+  end
+
+  describe "GET #destroy" do
+    it "deletes the post" do
+      delete :destroy, {id: @post.id}
+      count = Post.where({id: @post.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to posts index" do
+      delete :destroy, {id: @post.id}
+      expect(response).to redirect_to posts_path
+    end
+  end
 end
